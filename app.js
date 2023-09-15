2980a4d440705f752263956231e996e6ba059b33
SWATHINKTK <swathinktk10@gmail.com>
2023-09-15 19:39:42 +0530

    add all data

--- app.js
+++ app.js
@@ -11,21 +11,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/User',{useNewUrlParser:true,useUnifi
 const express = require('express');
 const app = express();
 
-const user = require("./controller/userController")
-
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
-
-
-
-app.get('/login',(req,res) => {
-    res.render('main',{title:"login"});
-})
-
-app.post('/login',user.insertUser);
-
-
-
+const userroute = require("./routres/userRouter");
+app.use('/',userroute)
 
 app.listen(5000,() => {
     console.log(`server is runnig at http://127.0.0.1:5000`);
--- controller/userController.js
+++ controller/userController.js
@@ -2,23 +2,21 @@ const userdetails = require("../models/userModel");
 
 const insertUser = async(req,res)=>{
     try {
-        console.log("insert")
-        console.log(req.body)
         const User = new userdetails({
             name:req.body.name,
             email:req.body.email,
             phone:req.body.phone,
             password:req.body.password,
         })
-        console.log(User)
-        console.log('Before saving user');
         const data = await User.save();
-        console.log('After saving user');
+        console.log('Registration Sucessful ....');
+        res.send("<h1>Registration Sucessfull ....</h1>"+data)
 
         
         
     } catch (error) {
-        res.send(error.msg)
+        res.send(error.msg);
+        console.log('Registration Faild !');
     }
 }
 
